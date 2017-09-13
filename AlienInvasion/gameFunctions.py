a7e534f08ede26ce44b37d3ec5eeed461b8dbc5e
Changchun Ye <chchye@nudt.edu.cn>
2017-09-13 17:29:44 +0800

    完成的功能： 1. 12.6节全部

--- AlienInvasion/gameFunctions.py
+++ AlienInvasion/gameFunctions.py
@@ -8,16 +8,25 @@ def check_events(ship):
             sys.exit()
         elif event.type == pygame.KEYDOWN:  #一直按下右箭头键，不会持续发送KEYDOWN类型的事件
             #print("check_event(), handle KEYDOWN")
-            if event.key == pygame.K_RIGHT:   #是event.key, 不是event.type
-                ship.moving_right = True
-                #print("check_event(), handle K_RIGHT")
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            on_key_down(event, ship)
         elif event.type == pygame.KEYUP:  #是KEYUP, 不是K_UP
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            on_key_up(event, ship)
+
+
+def on_key_up(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
+def on_key_down(event, ship):
+    if event.key == pygame.K_RIGHT:  # 是event.key, 不是event.type
+        ship.moving_right = True
+        # print("check_event(), handle K_RIGHT")
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
 
 def updateScreen(settings, screen, ship):
     # 设定屏幕背景色
